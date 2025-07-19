ba26e2ff09e263e404604315a3903f32f237c776
Mahender Singh <singhmahender1406@gmail.com>
2025-07-19 19:05:02 +0530

    minor change in backend for custom url

--- server/urlController.js
+++ server/urlController.js
@@ -1,5 +1,5 @@
-import {nanoid} from "nanoid";
-import {getUrlByOriginal,getUrlByShortCode,createShortUrl,incrementClickCount} from "./urlModel.js";
+import { nanoid } from "nanoid";
+import { getUrlByOriginal, getUrlByShortCode, createShortUrl, incrementClickCount } from "./urlModel.js";
 
 export const shortenUrl = async (req, res) => {
   const { original_url } = req.body;
@@ -7,15 +7,16 @@ export const shortenUrl = async (req, res) => {
     return res.status(400).json({ error: "original_url is required" });
   }
   try {
+    const baseUrl = process.env.BASE_URL || `${req.protocol}://${req.get("host")}`;
     //check if the long url already there
     const existing = await getUrlByOriginal(original_url);
     if (existing) {
-      return res.json({ short_code: `${req.protocol}://${req.get("host")}/${existing.short_code}` });
+      return res.json({ short_code: `${baseUrl}/${existing.short_code}` });
     }
     //generate new short code
     const short_code = nanoid(4); 
     const newEntry = await createShortUrl(original_url, short_code);
-    return res.status(201).json({ short_url: `${req.protocol}://${req.get("host")}/${short_code}` });
+    return res.status(201).json({ short_url: `${baseUrl}/${short_code}` });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ error: "Something went wrong" });
